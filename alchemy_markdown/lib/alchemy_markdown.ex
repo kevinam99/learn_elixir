a4f04fe5f09f3ff8aa5abc6f157db64b9a7b067f
Kevin Mathew <kevinam99@gmail.com>
2021-08-09 19:45:39 +0530

    init new proj

---
+++ alchemy_markdown/lib/alchemy_markdown.ex
@@ -0,0 +1,18 @@
+defmodule AlchemyMarkdown do
+  @moduledoc """
+  Documentation for `AlchemyMarkdown`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AlchemyMarkdown.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ alchemy_markdown/test/alchemy_markdown_test.exs
@@ -0,0 +1,8 @@
+defmodule AlchemyMarkdownTest do
+  use ExUnit.Case
+  doctest AlchemyMarkdown
+
+  test "greets the world" do
+    assert AlchemyMarkdown.hello() == :world
+  end
+end
