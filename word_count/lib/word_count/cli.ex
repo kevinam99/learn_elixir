a4696a78148b7c063b99bb106845da6adb6868f3
Kevin Mathew <kevinam99@gmail.com>
2021-08-09 22:01:19 +0530

    CLI complete

--- word_count/lib/word_count/cli.ex
+++ word_count/lib/word_count/cli.ex
@@ -1,11 +1,12 @@
 defmodule WordCount.CLI do
   def main(args) do
-    {parsed, args, invalid} = OptionParser.parse(
-      args,
-      switches: [chars: nil, lines: nil, words: nil],
-      aliases: [c: :chars, l: :lines, w: :words]
-    )
+    {parsed, args, invalid} =
+      OptionParser.parse(
+        args,
+        switches: [chars: nil, lines: nil, words: nil],
+        aliases: [c: :chars, l: :lines, w: :words]
+      )
 
-    IO.inspect {parsed, args, invalid}
+    WordCount.start(parsed, args, invalid)
   end
 end
