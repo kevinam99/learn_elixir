7325e171a939008bca2ae50643a3e9a981cbe8c2
Kevin Mathew <kevinam99@gmail.com>
2021-08-19 20:57:16 +0530

    Run migration and create schema

--- friends/config/config.exs
+++ friends/config/config.exs
@@ -1,10 +1,6 @@
 import Config
 
 config :friends, Friends.Repo,
-  database: "friends_repo",
-  username: "user",
-  password: "pass",
-  hostname: "localhost"
 
 config :friends, Friends.Repo,
   database: "friends_repo",
