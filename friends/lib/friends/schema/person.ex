ccd7d8ff9a503dba61c2a74773e4b25dc00e6396
Kevin Mathew <kevinam99@gmail.com>
2021-08-19 22:04:16 +0530

    Implement changeset

--- friends/lib/friends/schema/person.ex
+++ friends/lib/friends/schema/person.ex
@@ -1,10 +1,19 @@
 defmodule Friends.Schema.Person do
   use Ecto.Schema
+  import Ecto.Changeset
 
   schema "people" do
     field :name, :string, null: false
     field :age, :integer, default: 0
 
     timestamps()
+  end
+
+  def changeset(struct, params) do
+    struct
+    |> cast(params, [:name, :age])
+    |> validate_required([:name])
+    |> validate_length(:name, min: 2)
+    |> validate_number(:age, min: 18)
   end
 end
