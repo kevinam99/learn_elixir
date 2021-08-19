8d2e0d24b6e7b3f683b7054d1dfdd2bcce3f97a1
Kevin Mathew <kevinam99@gmail.com>
2021-08-19 22:00:18 +0530

    insert, list and remove friend

--- friends/lib/friends.ex
+++ friends/lib/friends.ex
@@ -1,4 +1,7 @@
 defmodule Friends do
+  alias Friends.Repo
+  alias Friends.Schema.Person
+  import Ecto.Query
   @moduledoc """
   Documentation for `Friends`.
   """
@@ -14,5 +17,25 @@ defmodule Friends do
   """
   def hello do
     :world
+  end
+
+  def insert(friend) do
+    Repo.insert!(friend)
+  end
+
+  def list_all_friends() do
+    query = from person in Person, select: {person.id, person.name, person.age}
+    Repo.all(query)
+  end
+
+  def update_something(id, updated_friend) do
+    friend = Repo.get!(Person, id) |> IO.inspect
+    friend = Ecto.Changeset.change friend, updated_friend |> IO.inspect
+    Repo.update(friend)
+  end
+
+  def remove_friend(id) do
+    friend = Repo.get! Person, id
+    Repo.delete!(friend)
   end
 end
