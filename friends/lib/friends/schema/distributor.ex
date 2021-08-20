611b8168b84ef624a0f06c424ede52d4b91db6a1
Kevin Mathew <kevinam99@gmail.com>
2021-08-20 07:45:03 +0530

    Migration and schema for distributors along with assoc.

---
+++ friends/lib/friends/schema/distributor.ex
@@ -0,0 +1,9 @@
+defmodule Friends.Schema.Distributor do
+  use Ecto.Schema
+
+  schema "distributors" do
+    field :name, :string
+    # field :movie_id, :integer
+    belongs_to(:movies, Friends.Schema.Movie)
+  end
+end
