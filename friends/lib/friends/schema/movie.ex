9548e8347457861b849c303460e1ed4aa7cf6679
Kevin Mathew <kevinam99@gmail.com>
2021-08-20 07:45:37 +0530

    Add has_one assoc with Distributor schema

--- friends/lib/friends/schema/movie.ex
+++ friends/lib/friends/schema/movie.ex
@@ -4,6 +4,7 @@ defmodule Friends.Schema.Movie do
   schema "movies" do
     field :title, :string
     field :tagline, :string
+    has_one(:distributor, Friends.Schema.Distributor)
     has_many(:characters, Friends.Schema.Character)
     # has_many/3 uses the foreign key on the associated schema, "characters", to make a movie’s associated
     # characters available. This is what will allow us to call movie.characters.
