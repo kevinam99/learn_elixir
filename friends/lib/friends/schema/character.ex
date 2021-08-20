aa7f0cb0996fe45643b0413e5897b3c56aa5246f
Kevin Mathew <kevinam99@gmail.com>
2021-08-20 07:29:11 +0530

    Create schema for movies and characters

---
+++ friends/lib/friends/schema/character.ex
@@ -0,0 +1,12 @@
+defmodule Friends.Schema.Character do
+  use Ecto.Schema
+
+  schema "characters" do
+    field :name, :string
+    belongs_to(:movies, Friends.Schema.Movie)
+
+    # In addition to adding the foreign key movie_id to our schema, it also gives us the ability to
+    # access associated movies schema through characters. It uses the foreign key
+    # to make a character’s associated movie available when we query for them. This is what will allow us to call character.movie.
+  end
+end
