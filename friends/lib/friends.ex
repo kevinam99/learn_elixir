3157bd9261ec54c71f95122b9b0a138ab5bc6835
Kevin Mathew <kevinam99@gmail.com>
2021-08-21 08:45:23 +0530

    Update test cases to work with index 1 of Friends.Repo db

--- friends/lib/friends.ex
+++ friends/lib/friends.ex
@@ -14,17 +14,17 @@ defmodule Friends do
       iex> Friends.insert(%Friends.Schema.Person{age: 91, name: "Somebody"})
       :ok
 
-      iex> Friends.update_friend(10, %{name: "name from mix test"})
+      iex> Friends.update_friend(1, %{name: "name from mix test"})
       :ok
 
-      iex> Friends.update_friend(10, %{name: "na"})
+      iex> Friends.update_friend(1, %{name: "na"})
       {:error, [
                   name: {"should be at least %{count} character(s)",
                   [count: 3, validation: :length, kind: :min, type: :string]}
                 ]
       }
 
-      iex> Friends.update_friend(10, %{age: 12})
+      iex> Friends.update_friend(1, %{age: 12})
       {:error,
           [
             age: {"must be greater than or equal to %{number}",
@@ -47,10 +47,7 @@ defmodule Friends do
 
   def list_all_friends() do
     query = from person in Person, select: {person.id, person.name, person.age}
-    case Repo.all(query) do
-      [] -> :ok
-      _ -> :error
-    end
+    Repo.all(query)
   end
 
   def update_friend(id, updated_friend) do
@@ -62,7 +59,7 @@ defmodule Friends do
     end
 
 
-    # funciton call -> update_friend 5, %{name: "K Mathew"}
+    # funciton call -> update_friend 1, %{name: "K Mathew"}
   end
 
   def remove_friend(id) do
