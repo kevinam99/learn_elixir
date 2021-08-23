e08a50596e81bae03abd6ce2240bd1c0df2352ba
Kevin Mathew <kevinam99@gmail.com>
2021-08-23 19:48:58 +0530

    New association: has_many(:bookmarked_links, through: [:bookmarks, :link])

--- linkly/lib/linkly/schemas/user.ex
+++ linkly/lib/linkly/schemas/user.ex
@@ -1,11 +1,13 @@
 defmodule Linkly.Schema.User do
   use Ecto.Schema
   alias Linkly.Schema.Bookmark
+
   schema "users" do
-    field :about, :string
-    field :username, :string
-    field :email, :string
+    field(:about, :string)
+    field(:username, :string)
+    field(:email, :string)
     has_many(:bookmarks, Bookmark)
+    has_many(:bookmarked_links, through: [:bookmarks, :link])
     timestamps()
   end
 end
