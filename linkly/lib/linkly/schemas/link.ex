7f1e57aa103b549ccc571f123d1363972d40e066
Kevin Mathew <kevinam99@gmail.com>
2021-08-23 16:03:47 +0530

    Add associations

--- linkly/lib/linkly/schemas/link.ex
+++ linkly/lib/linkly/schemas/link.ex
@@ -1,9 +1,9 @@
 defmodule Linkly.Schema.Link do
   use Ecto.Schema
-
+  alias Linkly.Schema.Bookmark
   schema "links" do
     field :url, :string
-
+    has_many(:bookmarks, Bookmark)
     timestamps()
   end
 end
--- linkly/lib/linkly/schemas/user.ex
+++ linkly/lib/linkly/schemas/user.ex
@@ -1,11 +1,11 @@
 defmodule Linkly.Schema.User do
   use Ecto.Schema
-
+  alias Linkly.Schema.Bookmark
   schema "users" do
     field :about, :string
     field :username, :string
     field :email, :string
-
+    has_many(:bookmarks, Bookmark)
     timestamps()
   end
 end
