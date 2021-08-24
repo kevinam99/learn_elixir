7f94b3826305ede8f39efaf552509cac31dfa106
Kevin Mathew <kevinam99@gmail.com>
2021-08-24 08:13:24 +0530

    Add new associations

--- linkly/lib/linkly/schemas/tag.ex
+++ linkly/lib/linkly/schemas/tag.ex
@@ -1,9 +1,12 @@
 defmodule Linkly.Schema.Tag do
   use Ecto.Schema
+  alias Linkly.Schema.{Link, LinkTag, User}
   # requires changeset
   schema "tags" do
     field(:title, :string)
-
+    has_many(:taggings, LinkTag)
+    many_to_many(:users, User, join_through: LinkTag)
+    many_to_many(:links, Link, join_through: LinkTag)
     timestamps()
   end
 end
--- linkly/lib/linkly/schemas/user.ex
+++ linkly/lib/linkly/schemas/user.ex
@@ -1,6 +1,6 @@
 defmodule Linkly.Schema.User do
   use Ecto.Schema
-  alias Linkly.Schema.Bookmark
+  alias Linkly.Schema.{Bookmark, Link, LinkTag, Tag}
 
   schema "users" do
     field(:about, :string)
@@ -8,6 +8,9 @@ defmodule Linkly.Schema.User do
     field(:email, :string)
     has_many(:bookmarks, Bookmark)
     has_many(:bookmarked_links, through: [:bookmarks, :link])
+    has_many(:taggings, LinkTag)
+    many_to_many(:tagged_links, Link, join_through: LinkTag)
+    many_to_many(:tags, Tag, join_through: LinkTag)
     timestamps()
   end
 end
