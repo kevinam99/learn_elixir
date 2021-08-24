cd81fe8fba6bc55601952ba7fc40658f085e9ca9
Kevin Mathew <kevinam99@gmail.com>
2021-08-24 08:12:19 +0530

    Remove fields and keep asscociations

--- linkly/lib/linkly/schemas/link_tag.ex
+++ linkly/lib/linkly/schemas/link_tag.ex
@@ -1,11 +1,14 @@
 defmodule Linkly.Schema.LinkTag do
   use Ecto.Schema
+  alias Linkly.Schema.{Link, Tag, User}
 
   schema "link_tags" do
-    field(:link_id, :id)
-    field(:tag_id, :id)
-    field(:user_id, :id)
-
+    # field(:link_id, :id)
+    # field(:tag_id, :id)
+    # field(:user_id, :id)
+    belongs_to(:link, Link)
+    belongs_to(:tag, Tag)
+    belongs_to(:user, User)
     timestamps()
   end
 end
