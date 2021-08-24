3a655a535905b6ac0813d060baf0933cf821a940
Kevin Mathew <kevinam99@gmail.com>
2021-08-24 08:12:50 +0530

    Remove unused alias and add associations

--- linkly/lib/linkly/schemas/link.ex
+++ linkly/lib/linkly/schemas/link.ex
@@ -1,10 +1,12 @@
 defmodule Linkly.Schema.Link do
   use Ecto.Schema
-  alias Linkly.Schema.Bookmark
+  alias Linkly.Schema.{Bookmark, LinkTag, Tag}
 
   schema "links" do
     field(:url, :string)
     has_many(:bookmarks, Bookmark)
+    has_many(:taggings, LinkTag)
+    many_to_many(:tags, Tag, join_through: LinkTag)
     timestamps()
   end
 end
