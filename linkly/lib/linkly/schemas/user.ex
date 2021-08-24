78bec5aa586dc9073906ed0a35e972e5fe70a54b
Kevin Mathew <kevinam99@gmail.com>
2021-08-24 08:58:33 +0530

    Add changest validations and constraints

--- linkly/lib/linkly/schemas/user.ex
+++ linkly/lib/linkly/schemas/user.ex
@@ -1,9 +1,11 @@
 defmodule Linkly.Schema.User do
   use Ecto.Schema
-  alias Linkly.Schema.{Bookmark, Link, LinkTag, Tag}
+  import Ecto.Changeset
+  alias Linkly.Schema.{Bookmark, Link, LinkTag, Tag, User}
 
   schema "users" do
     field(:about, :string)
+    field(:birth_date, :date, virtual: true)
     field(:username, :string)
     field(:email, :string)
     has_many(:bookmarks, Bookmark)
@@ -12,5 +14,25 @@ defmodule Linkly.Schema.User do
     many_to_many(:tagged_links, Link, join_through: LinkTag)
     many_to_many(:tags, Tag, join_through: LinkTag)
     timestamps()
+  end
+
+  def changeset(%User{} = user, attrs) do
+    user
+    |> cast(attrs, [:username, :about, :email, :birth_date])
+    |> validate_required([:username, :email, :birth_date])
+    |> validate_length(:username, min: 3)
+    |> validate_format(:email, ~r/@/)
+    |> validate_change(:birth_date, &older_than_13/2)
+    |> unique_constraint(:email)
+    |> unique_constraint(:username)
+  end
+
+  def older_than_13(:birth_date, %Date{} = birth_date) do
+    {year, month, date} = Date.to_erl(Date.utc_today())
+    min_date = Date.from_erl!({year - 13, month, date})
+    case Date.compare(min_date, birth_date) do
+      :lt -> [birth_date: "Must be over 13 years old!"]
+      _ -> []
+    end
   end
 end
